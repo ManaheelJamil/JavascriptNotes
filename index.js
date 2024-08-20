506ccfca6aacad3a87e1d52e1763aad6c776c059
Manaheel jamil <96910538+ManaheelJamil@users.noreply.github.com>
2024-08-20 12:50:41 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+// alert chapter  1
+
+// alert("Thanks for coming")
+
+// Variables for Strings
+
+var firstName = "Manaheel";
+
+firstName = "urooj"; //redeclare
+firstName = "malaika";
+// alert(firstName)
+
+var teachername;
+teachername = "manaheel";
+// alert(teachername)
+
+// Variables for Numbers
+
+var age = 20;
+// alert("my name is"+" " + teachername +" "+ "and I am" +" "+ age  +" "+"year old");
+alert(`My name is ${teachername} and I am ${age} year old`)  //ternary operators
+var age = age + 20
+alert(age)
+
+var additionone = 40;
+var additiontwo = 80;
+var total = additionone + additiontwo;
+alert(total)
